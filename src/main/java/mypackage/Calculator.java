56330a2c0af8fc3ec2c34d96a4bc284bea4f5861
vardhini <vardhinireddy22@gmail.com>
2022-10-09 15:47:19 +0530

    test

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -30,7 +30,7 @@ public class Calculator extends HttpServlet
         PrintWriter out= response.getWriter();
         int a1= Integer.parseInt(request.getParameter("n1"));
         int a2= Integer.parseInt(request.getParameter("n2"));
-        
+        //https://github.com/gaddampallivardhini/javaweb.git
         
         if(request.getParameter("r1")!=null)
         {
